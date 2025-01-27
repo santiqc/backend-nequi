e6436fb82988a77b84f28c3d35b7437d783ba059
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2025-01-27 15:56:09 -0500

    feat: cambio config de cors origin

--- src/main/java/com/nequi/franquicias/config/CorsConfig.java
+++ src/main/java/com/nequi/franquicias/config/CorsConfig.java
@@ -2,9 +2,13 @@ package com.nequi.franquicias.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.server.WebFilter;
+import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 
@@ -15,17 +19,37 @@ public class CorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Arrays.asList("*", "http://localhost:8080", "http://victorious-upliftment-production.up.railway.app"));
+        corsConfig.setAllowedOrigins(Arrays.asList("*"));
+        corsConfig.setAllowedMethods(Arrays.asList(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name(),
+                HttpMethod.OPTIONS.name(),
+                HttpMethod.PATCH.name()
+        ));
+        corsConfig.setAllowedHeaders(Arrays.asList(
+                HttpHeaders.AUTHORIZATION,
+                HttpHeaders.CONTENT_TYPE,
+                HttpHeaders.ACCEPT
+        ));
         corsConfig.setMaxAge(3600L);
-        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        corsConfig.setAllowedHeaders(Arrays.asList("*"));
-
         corsConfig.setAllowCredentials(false);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfig);
 
         return new CorsWebFilter(source);
+    }
+
+    @Bean
+    public WebFilter responseHeaderFilter() {
+        return (exchange, chain) -> {
+            return chain.filter(exchange)
+                    .then(Mono.fromRunnable(() -> {
+                        exchange.getResponse().getHeaders().add("Railway-Proxy-Pass", "true");
+                    }));
+        };
     }
 
 }
